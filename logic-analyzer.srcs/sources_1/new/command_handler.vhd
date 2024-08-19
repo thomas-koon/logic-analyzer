afc21a89ea4e243cefc62d965d65ea3b5f24ae0f
thomas-koon <tkoon@ucsd.edu>
2024-08-19 15:19:04 -0700

    command handler, not testbenched yet

--- logic-analyzer.srcs/sources_1/new/command_handler.vhd
+++ logic-analyzer.srcs/sources_1/new/command_handler.vhd
@@ -26,14 +26,86 @@ architecture behavioral of command_handler is
     signal curr_channels : std_logic_vector(CHANNELS - 1 downto 0);
 
     -- waiting for the second command to be parameter (after set mode/time)
-    signal pending_param : std_logic;
+    signal pending_mode_param : std_logic;
+    signal pending_target     : std_logic;
 
 begin
 
     process(clk, reset)
     begin
+        if reset = '1' then
 
-    end
+            curr_channels <= (others => '0');
+            pending_mode_param <= '0';
+            pending_target <= '0';
+            set_mode_param <= '0';
+            set_target <= '0';
+            param <= (others => '0');
+            channel_sel <= (others => '0');
+            mode <= (others => '0');
+            trigger <= (others => '0');
+            stop <= (others => '0');
+            channels_in <= (others => '0');
+
+        elsif rising_edge(clk) then
+
+            if pending_mode_param = '0' and pending_target = '0' then
+                set_mode_param <= '0';
+                set_target <= '0';
+                param <= "00000000";
+                channel_sel <= "0000";
+                mode <= "000";
+
+                case command(7:5) is
+
+                    -- activate channels
+                    when "001" =>
+
+                        channels_in <= command(4:1);
+
+                    -- set mode
+                    when "010" =>
+
+                        if to_integer(unsigned( command(4:3) )) < 4 then
+                            channel_sel(to_integer(unsigned( command(4:3) ))) <= '1';
+                            mode <= command(2:0);
+                        end if;
+
+                    -- set mode parameter
+                    -- parameter set in next cycle (in the else statement)
+                    when "011" =>
+
+                        if to_integer(unsigned( command(4:3) )) < 4 then
+                            channel_sel(to_integer(unsigned( command(4:3) ))) <= '1';
+                            pending_mode_param <= '1';
+                        end if;
+
+                    -- set target data amount
+                    when "100" =>
+
+                        channel_sel(to_integer(unsigned( command(4:3) ))) <= '1';
+                        pending_target <= '1';
+
+                end case;
+
+            -- if the last command was to set mode parameter
+            elsif pending_mode_param = '1' and pending_target = '0' then
+
+                set_mode_param <= '1';
+                param <= command;
+                pending_mode_param <= '0';
+
+            -- if the last command was to set target parameter
+            elsif pending_mode_param = '0' and pending_target = '1' then
+
+                set_target <= '1';
+                param <= command;
+                pending_target <= '0';
+            end if;
+
+        end if;
+
+    end process;
 
 
 
